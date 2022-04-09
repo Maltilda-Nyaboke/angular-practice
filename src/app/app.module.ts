dc6cbc0206993c58da3452d380bb23eef70534f9
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-09 11:43:23 +0300

    :test-angular-deployment

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Routes } from '@angular/router';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+
+// const appRoutes:Routes =[
+//   {path:'home'}
+// ]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
